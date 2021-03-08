117a62e27e39ba9cfa6e130e28e38162b6f41a15
Khalil Mohamed <mohamed.khalil@he-arc.ch>
2021-03-08 15:48:04 +0100

    creation of the models (WIP)

--- dtc/dtcapp/models.py
+++ dtc/dtcapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class LikedClip(models.Model):
+    clipURL = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.clipURL
+
+
+class Streamer(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name + " " + str(self.image)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    # voir ici pour mdp plus tard : https://stackoverflow.com/questions/17523263/how-to-create-password-field-in-model-django
+    password = models.CharField(max_length=50)
+    Likes = models.ManyToManyField(LikedClip)
+    Follows = models.ManyToManyField(Streamer)
+    Subscriptions = models.ManyToManyField('self')
+
+    def __str__(self):
+        return self.username
