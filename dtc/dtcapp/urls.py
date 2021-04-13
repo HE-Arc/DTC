de61071caede1e562df515a412f7ebac83b041af
cedric.campos <cedric.camposcarvalho@he-arc.ch>
2021-04-13 10:56:25 +0200

    Revue Mi-projet comments fixed

--- dtc/dtcapp/urls.py
+++ dtc/dtcapp/urls.py
@@ -3,13 +3,6 @@ from django.contrib import admin
 
 from . import views
 
-# TODO-ADV-1-3 Uncomment thos four lines used by restframework
-#from rest_framework import routers
-#router = routers.DefaultRouter()
-#router.register('users', views.UserViewSet)
-#router.register('soldiers', views.SoldierViewSet)
-
-
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('', views.index, name='index'),
